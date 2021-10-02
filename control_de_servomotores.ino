c9dd163cb4718b39bfc88499450b4d891a885260
Aldo Danilo Vargas Esquivel <84344962+Danilo-Vargas@users.noreply.github.com>
2021-10-01 20:35:12 -0500

    Update control_de_servomotores.ino

--- control_de_servomotores.ino
+++ control_de_servomotores.ino
@@ -1,22 +1,37 @@
+
+// CONTROL DE SERVOMOTORES - Aldo Danilo Vargas Esquivel - 01/10/2021
+
+// INCLUIMOS LA LIBRERIA Servo.h
+
 #include <Servo.h>
+
+// DECLARAMOS LOS PINES
 
 #define pinServo_1 11
 #define pinServo_2 12
 #define pinPot_1 A0
 #define pinPot_2 A1
 
+// CREAMOS LOS OBJETOS Servo
+
+    // UNO PARA CADA SERVOMOTOR
+
 Servo servo_1;
 Servo servo_2;
+
+// INICIAMOS LAS VARIABLES, ESTAS REPRESENTAN EL ANGULO DE CADA MOTOR
 
 int deg_1 = 0;
 int deg_2 = 0;
 
-void readServoState(){
+// INICIAMOS LAS FUNCIONES
+
+void readServoState(){ // LEE EL VALOR DE LOS SENSORES(POTENCIOMETROS) Y GUARDA EL VALOR EN LAS VARIABLES CORRESPONDIENTES
     deg_1 = analogRead(pinPot_1);
     deg_2 = analogRead(pinPot_2);
 }
 
-void setServoState(){
+void setServoState(){ // ESTABLECE LA POSICION DE CADA SERVOMOTOR E IMPRIME EN PANTALLA LOS VALORES DE CADA UNO
 
     readServoState();
 
@@ -31,10 +46,12 @@ void setServoState(){
     delay(10);
 }
 
+// SETUP
+
 void setup()
 {
 
-  Serial.begin(9600);
+    Serial.begin(9600);
   
     pinMode(pinPot_1, INPUT);
     pinMode(pinPot_2, INPUT);
@@ -47,7 +64,9 @@ void setup()
     delay(1500);
 }
 
-void loop()
+// LOOP
+
+void loop() // EN EL LOOP SOLO SE EJECUTA LA FUNCION setServoState() Y QUEDA MUY LIMPIO EL CODIGO
 {
     setServoState();
 }
